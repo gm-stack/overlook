ab0c2e4b8246ef4e820e1db917904f7b70fd24bf
gm <gm@6219c1a7-eedc-4b83-93ca-c1744c1638e7>
2012-12-30 13:41:59 +0000

    many updates...
    git-svn-id: http://svn.stackunderflow.com/svn/public/overlook@284 6219c1a7-eedc-4b83-93ca-c1744c1638e7

--- doorlock.py
+++ doorlock.py
@@ -2,43 +2,84 @@
 
 import serial
 import RPi.GPIO as GPIO
-import time, io
+import time, io, datetime
 import MySQLdb, MySQLdb.cursors
 import ConfigParser
+import asyncore
+import thread
+from ircasync import *
 
 config = ConfigParser.ConfigParser()
 config.read("doorlock.ini")
 
-GPIO_PIN = config.getint("gpio","gpio_pin")
+LOCK_PIN = config.getint("gpio","lock_pin")
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False) # disable warning for already having GPIO open
-GPIO.setup(GPIO_PIN, GPIO.OUT)
-GPIO.output(GPIO_PIN, GPIO.LOW)
+GPIO.setup(LOCK_PIN, GPIO.OUT)
+GPIO.output(LOCK_PIN, GPIO.LOW)
+
+EXIT_PIN = config.getint("gpio","exit_pin")
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(EXIT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 ser = serial.Serial(config.get("rfid","port"), config.getint("rfid","baud"), timeout=1)
 
+print "connecting to MySQL"
 conn = MySQLdb.connect(	host=config.get("mysql","host"),
 			user=config.get("mysql","user"),
 			passwd=config.get("mysql","passwd"),
 			db=config.get("mysql","db"), 
 			cursorclass=MySQLdb.cursors.DictCursor, charset='utf8')
 cursor = conn.cursor()
+print "MySQL connected"
+opentime = config.getint("gpio","opentime")
 
-def toggleDoor():
-	GPIO.output(GPIO_PIN, GPIO.HIGH)
-	time.sleep(2)
-	GPIO.output(GPIO_PIN, GPIO.LOW)
+doorTime = 0.0
+def doorThread():
+	global doorTime
+	while True:
+		if GPIO.input(EXIT_PIN) and not ((time.time() - doorTime) < 0):
+			doorTime = time.time() + 2
+			print "button pushed, unlocking door"
+			try:
+				irc.tell(channel, "Unlocking door for exiting user")
+			except:
+				pass
+		if (time.time() - doorTime) < 0:
+			GPIO.output(LOCK_PIN, GPIO.HIGH)
+		else:
+			GPIO.output(LOCK_PIN, GPIO.LOW)
+		time.sleep(0.05)
+thread.start_new_thread(doorThread, ())
+
+irc_connected = False
+def handle_welcome(_,__):
+	global irc_connected
+	irc_connected = True
+
+channel = config.get("irc","channel")
+irc = IRC(nick=config.get("irc","ircnick"), start_channels=[channel], version="1.0")
+irc.make_conn(config.get("irc","ircserver"), config.getint("irc","ircport"))
+irc.bind(handle_welcome, RPL_WELCOME)
+thread.start_new_thread(asyncore.loop, ()) # i really should work the serial bit into asyncore, shouldn't i?
 
 def checkCard(tagID):
-	cursor.execute("SELECT * FROM `rfid` WHERE `card_id`=%(tagID)s", {'tagID': tagID})
+	global doorTime
+	cursor.execute("SELECT * FROM `rfid` WHERE `card_id`=%(cardid)s", {'cardid': tagID})
 	result = cursor.fetchone()
 	if result:
 		if result['enabled']:
 			print "valid card, unlocking door for %s" % result['username']
+			try:
+				irc.tell(channel, ("Unlocking door for user %s" % result['username']).encode("ascii",errors="ignore"))
+			except:
+				print sys.exc_info()
 			if result['soundfile']:
 				print "soundfile is %s" % result['soundfile']
-			toggleDoor()
+			doorTime = time.time() + opentime
 		else:
 			print "usage of disabled card %s for user %s" % (result['card_id'], result['username'])
 	else:
@@ -66,7 +107,8 @@ while True:
 			print "invalid checksum"
 			continue
 		print "got card, ID is %s" % databytes
-		checkCard(tagID)
+		if tagID != "0000000000": # RFID reader sometimes spits out all zeros for some reason, but this passes checksum because 0 ^ 0 = 0
+			checkCard(tagID)
 	else:
 		if len(data) > 0: print "invalid read len %i" % len(data)
 
