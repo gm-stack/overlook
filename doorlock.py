74d9d75dfc1f2de1fe20632f476cb44e3e1a5634
gm <gm@6219c1a7-eedc-4b83-93ca-c1744c1638e7>
2013-01-12 07:33:55 +0000

    added rdm880 and 13.56 card support. auths by card ID so far
    git-svn-id: http://svn.stackunderflow.com/svn/public/overlook@285 6219c1a7-eedc-4b83-93ca-c1744c1638e7

--- doorlock.py
+++ doorlock.py
@@ -7,7 +7,12 @@ import MySQLdb, MySQLdb.cursors
 import ConfigParser
 import asyncore
 import thread
+import pygame
+import pyaudio, wave
+import os
+import random
 from ircasync import *
+from rdm880 import *
 
 config = ConfigParser.ConfigParser()
 config.read("doorlock.ini")
@@ -20,10 +25,12 @@ GPIO.setup(LOCK_PIN, GPIO.OUT)
 GPIO.output(LOCK_PIN, GPIO.LOW)
 
 EXIT_PIN = config.getint("gpio","exit_pin")
+FRIDGE_PIN = config.getint("gpio","fridge_pin")
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(EXIT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(FRIDGE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 ser = serial.Serial(config.get("rfid","port"), config.getint("rfid","baud"), timeout=1)
 
@@ -37,9 +44,25 @@ cursor = conn.cursor()
 print "MySQL connected"
 opentime = config.getint("gpio","opentime")
 
+pygame.mixer.pre_init(44100,-16,2,256)
+pygame.mixer.init()
+
+sounds = {}
+wavlist = os.listdir("sounds")
+for wav in wavlist:
+	if wav.endswith(".wav"):
+		sounds[wav] = pygame.mixer.Sound("sounds/" + wav)
+
+ping = pygame.mixer.Sound("Ping.wav")
+ping.play()
+
+
 doorTime = 0.0
+fridgebell = pygame.mixer.Sound("bell.wav")
+fridgebell_playing = False
 def doorThread():
 	global doorTime
+	fridgeTime = 0
 	while True:
 		if GPIO.input(EXIT_PIN) and not ((time.time() - doorTime) < 0):
 			doorTime = time.time() + 2
@@ -52,6 +75,15 @@ def doorThread():
 			GPIO.output(LOCK_PIN, GPIO.HIGH)
 		else:
 			GPIO.output(LOCK_PIN, GPIO.LOW)
+		if GPIO.input(FRIDGE_PIN):
+			fridgeTime += 1
+			print "fridge has been open for %f seconds" % (fridgeTime/20.0)
+			if fridgeTime == 30*20:
+				print "playing bell"
+				fridgebell.play(loops=-1)
+		else:
+			fridgeTime = 0.0
+			fridgebell.stop()
 		time.sleep(0.05)
 thread.start_new_thread(doorThread, ())
 
@@ -72,6 +104,7 @@ def checkCard(tagID):
 	result = cursor.fetchone()
 	if result:
 		if result['enabled']:
+			random.choice(sounds.values()).play()
 			print "valid card, unlocking door for %s" % result['username']
 			try:
 				irc.tell(channel, ("Unlocking door for user %s" % result['username']).encode("ascii",errors="ignore"))
@@ -85,30 +118,44 @@ def checkCard(tagID):
 	else:
 		print "unknown card %s" % tagID
 
+prevcardid = None
 while True:
-	data = ser.read(14)
-	if len(data) == 14:
-		if ord(data[0]) != 2:
-			print "invalid header"
-			continue
-		if ord(data[13]) != 3:
-			print "invalid footer"
-			continue
-		tagID = data[1:11]
-		checksum = int(data[11:13],16)
-		databytes = []
-		for i in range(0,10,2):
-			tagbyte = tagID[i:i+2]
-			databytes += [int(tagbyte,16)]
-		calcchecksum = 0
-		for byte in databytes:
-			calcchecksum = calcchecksum ^ byte
-		if (calcchecksum != checksum):
-			print "invalid checksum"
-			continue
-		print "got card, ID is %s" % databytes
-		if tagID != "0000000000": # RFID reader sometimes spits out all zeros for some reason, but this passes checksum because 0 ^ 0 = 0
-			checkCard(tagID)
+	p = Packet(ISO14443A.Request)
+	reply = p.execute(ser)
+	p = Packet(ISO14443A.Anticollision)
+	reply = p.execute(ser)
+	cardid = reply.data
+	if cardid:
+		cardid_hex = "".join(map(lambda x: "%.2X" % x , cardid))
+		if cardid_hex != prevcardid:
+			prevcardid = cardid_hex
+			checkCard(cardid_hex)
 	else:
-		if len(data) > 0: print "invalid read len %i" % len(data)
+		prevcardid = None
+	time.sleep(0.1)
+	#data = ser.read(14)
+	#if len(data) == 14:
+	#	if ord(data[0]) != 2:
+	#		print "invalid header"
+	#		continue
+	#	if ord(data[13]) != 3:
+	#		print "invalid footer"
+	#		continue
+	#	tagID = data[1:11]
+	#	checksum = int(data[11:13],16)
+	#	databytes = []
+	#	for i in range(0,10,2):
+	#		tagbyte = tagID[i:i+2]
+	#		databytes += [int(tagbyte,16)]
+	#	calcchecksum = 0
+	#	for byte in databytes:
+	#		calcchecksum = calcchecksum ^ byte
+	#	if (calcchecksum != checksum):
+	#		print "invalid checksum"
+	#		continue
+	#	print "got card, ID is %s" % databytes
+	#	if tagID != "0000000000": # RFID reader sometimes spits out all zeros for some reason, but this passes checksum because 0 ^ 0 = 0
+	#		checkCard(tagID)
+	#else:
+	#	if len(data) > 0: print "invalid read len %i" % len(data)
 
