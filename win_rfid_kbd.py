c29d1547e054c6cc00645eab1f8922e25e670026
gm <gm@6219c1a7-eedc-4b83-93ca-c1744c1638e7>
2013-01-13 04:30:59 +0000

    reconnect serial
    git-svn-id: http://svn.stackunderflow.com/svn/public/overlook@299 6219c1a7-eedc-4b83-93ca-c1744c1638e7

--- win_rfid_kbd.py
+++ win_rfid_kbd.py
@@ -18,8 +18,6 @@ conn = MySQLdb.connect(	host=config.get("mysql","host"),
 cursor = conn.cursor()
 print "connected"
 
-io = Serial('COM4', 9600, timeout=1)
-
 shell = win32com.client.Dispatch("WScript.Shell")
 
 def checkCard(cardid_hex):
@@ -31,27 +29,26 @@ def checkCard(cardid_hex):
 
 while True:
 	try:
-		p = Packet(ISO14443A.Request)
-		reply = p.execute(io)
-		p = Packet(ISO14443A.Anticollision)
-		reply = p.execute(io)
-		cardid = reply.data
-		if cardid:
-			cardid_hex = "".join(map(lambda x: "%.2X" % x , cardid))
-			if cardid_hex != prevcardid:
-				prevcardid = cardid_hex
-				checkCard(cardid_hex)
-		else:
-			prevcardid = None
-		time.sleep(0.1)
-	except KeyboardInterrupt:
-		sys.exit()
-	except serial.SerialException:
-		print "serial error, re-opening"
-		time.sleep(1)
-		try:
-			io = Serial('COM4', 9600, timeout=1)
-		except:
-			pass
-	except:
-		print sys.exc_info()[0]
+		io = Serial('COM4', 9600, timeout=1)
+		while True:
+			try:
+				p = Packet(ISO14443A.Request)
+				reply = p.execute(io)
+				p = Packet(ISO14443A.Anticollision)
+				reply = p.execute(io)
+				cardid = reply.data
+				if cardid:
+					cardid_hex = "".join(map(lambda x: "%.2X" % x , cardid))
+					if cardid_hex != prevcardid:
+						prevcardid = cardid_hex
+						checkCard(cardid_hex)
+				else:
+					prevcardid = None
+				time.sleep(0.1)
+			except KeyboardInterrupt:
+				sys.exit()
+			except serial.SerialException:
+				print "serial error, re-opening"
+				break
+			except:
+				print sys.exc_info()[0]
